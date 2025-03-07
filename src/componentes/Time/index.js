5e558e858f6ae91df7f12c4005815a920f6da8f0
Alberto Oliveira <alberto.olv00@gmail.com>
2025-03-07 12:41:07 -0300

    Alterar cor colaborador

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -4,6 +4,7 @@ import './Time.css'
 const Time = ({ time, colaboradores, aoDeletar }) => {
     return (
         colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: time.corPrimaria }}>
+            <input value={time.corSecundaria76} type='color' className='input-cor' />
             <h3 style={{ borderColor: time.corSecundaria }}>{time.nome}</h3>
             <div className='colaboradores'>
                 {colaboradores.map((colaborador, indice) => {             
